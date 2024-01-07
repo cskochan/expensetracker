d44a382523a4d3a28e4172bedd7a9799064192b5
Cameron <cskochan@gmail.com>
2024-01-06 16:42:06 -0800

    Added ability to create new csv files according to year_month.csv format, and return a list of existing csv files with that format

--- expensetracker.py
+++ expensetracker.py
@@ -1,24 +1,27 @@
+import os
+import re
 from datetime import datetime
 
 def main():
-    
-    # check for latest csv and read current budget. if no csv, ask for budget
-    csv_exists = False
-    if csv_exists == False:
-        budget = input("How much you currently have: ")
-        print("You have $" + budget)
+    # finds most recent expense sheet month for current acct balance info
+    path = ("./monthly_expense_files/")
+    files = [f for f in os.listdir(path) if re.search(r'\d{4}_\d{2}.csv$', f)]
+    try:
+        # balance = sorted(files)[-1] along with whatever info we need from
+        # the .csv to retrieve the balance
+        print(sorted(files)[-1])
+    except:
+        # no file means we ask for balance and create a new file
+        print("Looks like you're new here. Lets get you started.")
+        date_input()
+        exp_name = "Balance"
+        exp_type = "Balance"
+        balance = input("How much you currently have: ")
+        print("You now have an acct balance of $" + balance + ".")
+
         
-    # Date input. Repeats until input is valid.
-    # Validity is critical because date is used to create and search csv file names
-    date_is_valid = False
-    while(date_is_valid == False):
-        exp_date = input ("Date of Purchase [MM/DD/YYYY]: ")
-        datetime_obj = date_check(exp_date)
-        if datetime_obj != None:
-            date_is_valid = True
-    file_check(exp_date)
-    print(datetime_obj.year)
-    
+
+    date_input()
     # option to add either an expense or an income
     picker = False
     while(picker == False):
@@ -29,12 +32,14 @@ def main():
             exp_type = input("Which type of expense: ")
             exp_amount = input("Expense amount: ")
             picker = True
+            # SUBTRACT EXPENSE TO .CSV HERE
             print("Expense added")
         elif is_expense == "2":
             exp_name = input("Income source: ")
             exp_type = input("One time or regular: ")
             exp_amount = input("Income amount: ")
             picker = True
+            # ADD INCOME TO .CSV HERE
             print("Input added")
 
     print(exp_name + ", " + exp_type + ", " + exp_amount)
@@ -45,6 +50,17 @@ def main():
     # options to include any useful comparisons
         # ex/ last 6 months with the same period last year
         
+# Repeats until input is valid.
+# Validity is critical because date is used to create and search csv file names
+def date_input():
+    date_is_valid = False
+    while(date_is_valid == False):
+        exp_date = input ("Date of Purchase [MM/DD/YYYY]: ")
+        exp_datetime = date_check(exp_date)
+        if exp_datetime != None:
+            date_is_valid = True
+    filename = str(exp_datetime.year) + "_" + str(exp_datetime.month) + ".csv"
+    open_file(filename)
 
 # Checks that date is valid. If so, return Datetime Object. Else return None
 def date_check(date) -> datetime:
@@ -56,10 +72,9 @@ def date_check(date) -> datetime:
         return None
         
 
-def file_check(date) -> None:
-    # check to make sure file for that month and year exist, if so, open it
-    # if not, create it
-    print(date)
+def open_file(filename) -> None:
+    stream = open("./monthly_expense_files/" + filename, mode='w+t', encoding=None)
+    print(filename)
     pass
 
 if __name__== "__main__":
