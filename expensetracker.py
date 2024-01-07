abeaf359c149e07ddee5a889f082dfbdeb465179
Cameron <cskochan@gmail.com>
2024-01-06 17:30:00 -0800

    write to csv file if it is brand new, and read data from it if it is not. Rearranged stream opening functionality

--- expensetracker.py
+++ expensetracker.py
@@ -4,23 +4,27 @@ from datetime import datetime
 
 def main():
     # finds most recent expense sheet month for current acct balance info
+    balance = 0
     path = ("./monthly_expense_files/")
     files = [f for f in os.listdir(path) if re.search(r'\d{4}_\d{2}.csv$', f)]
     try:
         # balance = sorted(files)[-1] along with whatever info we need from
         # the .csv to retrieve the balance
-        print(sorted(files)[-1])
-    except:
+        for line in open(path + sorted(files)[-1], mode = "r+t", encoding = None):
+            line_list = line.split(", ")
+            if line_list[1] == "Balance":
+                balance = line_list[2]
+    except Exception as e:
         # no file means we ask for balance and create a new file
+        print(e)
         print("Looks like you're new here. Lets get you started.")
-        date_input()
+        filename = (date_input())
         exp_name = "Balance"
         exp_type = "Balance"
         balance = input("How much you currently have: ")
-        print("You now have an acct balance of $" + balance + ".")
-
+        write_to_file(filename, exp_name + ", " + exp_type + ", " + balance)
+    print("You have an acct balance of $ " + balance + ".")
         
-
     date_input()
     # option to add either an expense or an income
     picker = False
@@ -29,6 +33,7 @@ def main():
         is_expense = input("Expense or Income [1 or 2]:")
         if is_expense == "1":
             exp_name = input("What you just bought: ")
+            print("""1. Food\n2. Shelter\n3. Fun\n 4. Misc""")
             exp_type = input("Which type of expense: ")
             exp_amount = input("Expense amount: ")
             picker = True
@@ -36,11 +41,13 @@ def main():
             print("Expense added")
         elif is_expense == "2":
             exp_name = input("Income source: ")
+            print("""1. One time\n2. Regular""")
             exp_type = input("One time or regular: ")
             exp_amount = input("Income amount: ")
             picker = True
             # ADD INCOME TO .CSV HERE
             print("Input added")
+
 
     print(exp_name + ", " + exp_type + ", " + exp_amount)
     # add this information to the open csv file
@@ -52,7 +59,7 @@ def main():
         
 # Repeats until input is valid.
 # Validity is critical because date is used to create and search csv file names
-def date_input():
+def date_input() -> str:
     date_is_valid = False
     while(date_is_valid == False):
         exp_date = input ("Date of Purchase [MM/DD/YYYY]: ")
@@ -60,7 +67,7 @@ def date_input():
         if exp_datetime != None:
             date_is_valid = True
     filename = str(exp_datetime.year) + "_" + str(exp_datetime.month) + ".csv"
-    open_file(filename)
+    return filename
 
 # Checks that date is valid. If so, return Datetime Object. Else return None
 def date_check(date) -> datetime:
@@ -72,10 +79,10 @@ def date_check(date) -> datetime:
         return None
         
 
-def open_file(filename) -> None:
+def write_to_file(filename, content):
     stream = open("./monthly_expense_files/" + filename, mode='w+t', encoding=None)
-    print(filename)
-    pass
+    stream.write(content)
+    stream.close()
 
 if __name__== "__main__":
     main()
