8334e31e05e52e419945945843d112b1b26d4be3
Cameron <cskochan@gmail.com>
2024-01-07 16:14:10 -0800

    Added ability to append expense/income to .csv file, and moved code to do so into functions to make main() easier to understand.

--- expensetracker.py
+++ expensetracker.py
@@ -5,67 +5,115 @@ from datetime import datetime
 def main():
     # finds most recent expense sheet month for current acct balance info
     balance = 0
+    oldest_record = ""
+    newest_record = ""
     path = ("./monthly_expense_files/")
     files = [f for f in os.listdir(path) if re.search(r'\d{4}_\d{2}.csv$', f)]
+    
     try:
         # balance = sorted(files)[-1] along with whatever info we need from
         # the .csv to retrieve the balance
-        for line in open(path + sorted(files)[-1], mode = "r+t", encoding = None):
+        file_list = sorted(files)
+        print(file_list)
+        oldest_record = file_list[0]
+        print(oldest_record)
+        newest_record = file_list[-1]
+        print(newest_record)
+        for line in open(path + newest_record, mode = "r+t", encoding = None):
             line_list = line.split(", ")
-            if line_list[1] == "Balance":
-                balance = line_list[2]
+            if line_list[2] == "Balance":
+                balance = line_list[3]
     except Exception as e:
         # no file means we ask for balance and create a new file
         print(e)
         print("Looks like you're new here. Lets get you started.")
-        filename = (date_input())
-        exp_name = "Balance"
-        exp_type = "Balance"
+        change_date = date_input("new")
+        filename = create_filename(change_date)
+        name = "Starting"
+        type = "Balance"
         balance = input("How much you currently have: ")
-        write_to_file(filename, exp_name + ", " + exp_type + ", " + balance)
-    print("You have an acct balance of $ " + balance + ".")
+        exp_or_in = "Balance" #expense or income, uniquely named balance as this is initial balance for month
+        write_to_file(filename, str(change_date.date()) + ", " + name + ", " + type + ", " + str(balance) + ", " + exp_or_in)
+    print("You have an acct balance of $ " + str(balance) + ".")
         
-    date_input()
+    
+    # if date is for a new month, need to write something that collects balance from
+    # previous month.
+    # Also consider that if then that previous month has a new expense or income
+    # added to it, it will change the balance for that month, and the subsequent
+    # months will also have to be adjusted accordingly.
+    
     # option to add either an expense or an income
-    picker = False
-    while(picker == False):
-        print("""1. Expense\n2. Income""")
-        is_expense = input("Expense or Income [1 or 2]:")
-        if is_expense == "1":
-            exp_name = input("What you just bought: ")
-            print("""1. Food\n2. Shelter\n3. Fun\n 4. Misc""")
-            exp_type = input("Which type of expense: ")
-            exp_amount = input("Expense amount: ")
-            picker = True
-            # SUBTRACT EXPENSE TO .CSV HERE
-            print("Expense added")
-        elif is_expense == "2":
-            exp_name = input("Income source: ")
-            print("""1. One time\n2. Regular""")
-            exp_type = input("One time or regular: ")
-            exp_amount = input("Income amount: ")
-            picker = True
-            # ADD INCOME TO .CSV HERE
-            print("Input added")
-
-
-    print(exp_name + ", " + exp_type + ", " + exp_amount)
+    picker = True
+    while(picker == True):
+        picker = new_entry()
+    
     # add this information to the open csv file
     
     # ask if there are any more expenses to add
     # if not, print a summary of expenses breakdown, remaining budget, etc
     # options to include any useful comparisons
         # ex/ last 6 months with the same period last year
+    
+def new_entry() -> bool:
+    adj_date = date_input()
+    filename = create_filename(adj_date)
+    print("""1. Expense\n2. Income""")
+    adjustment_type = input("Expense or Income [1 or 2]:")
+    if adjustment_type == "1":
+        adjustment_str = append_expense(adj_date)
+    elif adjustment_type == "2":
+        adjustment_str = append_income(adj_date)
+    write_to_file(filename, adjustment_str)
+    if input("New entry? [Y/n]: ") != "Y" or "y":
+        print("Thank you! See you next time.")
+        return False
+    else:
+        return True
+        
+def append_expense(adj_date) -> str:
+    name = input("What you just bought: ")
+    print("""1. Food\n2. Shelter\n3. Fun\n4. Misc""")
+    type = input("Which type of expense: ")
+    amount = input("Expense amount: ")
+    exp_or_in = "Expense"
+    adjust_balance(amount * -1)
+    print("Expense added")
+    return str(adj_date.date()) + ", " + name + ", " + type + ", " + str(amount) + ", " + exp_or_in
+
+def append_income(adj_date) -> str:
+    name = input("Income source: ")
+    print("""1. One time\n2. Regular""")
+    type = input("One time or regular: ")
+    amount = input("Income amount: ") 
+    exp_or_in = "Income"
+    adjust_balance(amount)
+    print("Input added")
+    return str(adj_date.date()) + ", " + name + ", " + type + ", " + str(amount) + ", " + exp_or_in
+
+def adjust_balance(amount):
+    
+    pass
+
+    
+
         
 # Repeats until input is valid.
 # Validity is critical because date is used to create and search csv file names
-def date_input() -> str:
+def date_input(new = "") -> datetime:
     date_is_valid = False
     while(date_is_valid == False):
-        exp_date = input ("Date of Purchase [MM/DD/YYYY]: ")
+        if new == "new":
+            exp_date = input("Today's date [MM/DD/YYYY]: ")
+        else:
+            exp_date = input("Date of Purchase [MM/DD/YYYY]: ")
         exp_datetime = date_check(exp_date)
         if exp_datetime != None:
             date_is_valid = True
+    return exp_datetime
+
+# File name format: YYYY_MM.csv
+def create_filename(exp_datetime) -> str:
     filename = str(exp_datetime.year) + "_" + str(exp_datetime.month) + ".csv"
     return filename
 
@@ -80,9 +128,10 @@ def date_check(date) -> datetime:
         
 
 def write_to_file(filename, content):
-    stream = open("./monthly_expense_files/" + filename, mode='w+t', encoding=None)
-    stream.write(content)
+    stream = open("./monthly_expense_files/" + filename, mode='a', encoding=None)
+    stream.write(content + "\n")
     stream.close()
+    print("File updated")
 
 if __name__== "__main__":
     main()
