614df0c970bb8a0ec035e7ad4203904c078a8131
Cameron <cskochan@gmail.com>
2024-02-06 16:01:48 -0800

    Added ability to save files to middle of documents while adjusting subsequent ones accordingly. Had to rewrite various sections of code. Also did a bit of cleaning up of dead code and unnecessary commentary.

--- expensetracker2.py
+++ expensetracker2.py
@@ -6,10 +6,8 @@ from datetime import date, datetime
 path = ("./monthly_expense_files/")
 yearList = sorted([f for f in os.listdir(path) if re.search(r'\d{4}.csv$', f)])
 def main():
-    
     balance = {}
     entry = {}
-    
     balance = welcome(balance)
     while(True):
         if entryTest() == True:
@@ -18,42 +16,24 @@ def main():
             else:
                 entry = newIncome(entry)
             writeEntry(monthFilename(entry), entry)
-            prevBalance = readLine(getPrevBalSheet(yearFilename(entry)), dateToFirst(entry['date']))
-            print(prevBalance)
-            newBalance = makeNewBalDict(entry, prevBalance)
             
-            # the below writeLine() call only appends the file, doesn't replace.
-            # i'll write the adjustBalances() function, and have it do
-            # this part at the same time.
-            #writeLine(path + yearFilename(newBalance['date']), newBalance, entry['amount'])
-            # have to find subsequent entries and adjust them with new information
-            
-            writeBalance(yearFilename(newBalance), newBalance, entry['amount'])
+            yearFile = yearFilename(entry)
+            if yearFile not in yearList:
+                balSheetIndex = createNewBalSheet(yearFile)
+                newRow = readLine(balSheetIndex-1, dateToFirst(entry['date']), True)
+            else:
+                balSheetIndex = yearList.index(yearFile)
+                newRow = readLine(balSheetIndex, dateToFirst(entry['date']), True)
+            newRow = updateRow(entry, newRow)
+            writeBalance(balSheetIndex, newRow, entry['amount'])
         else:
             break
-
-        # get current balance
-        # get user input for next entry
-        # calculate new balance
-        # add entry
-            # 1. (current month)
-                # add to existing file
-            # 2. (new month)
-                # create new file and add to that
-                # entry to yearly file
-                    # 1. (current year)
-                        # add to existing file
-                    # 2. (new year)
-                        # create new file and add to that
-            # 3. (old month)
-                # add to existing month file
-                # adjust balance for that month and all subsequent months
-                    # check for subsequent year files and adjust those too
-        # if no more entrys ask about ways to display existing data
-            # expenses for month by type, monthly comparisons, etc
-    print("Goodbye.")
+    balance = readLine(-1, needPrev = True)
+    print("Your current balance is now: $ ", + str("{:.02f}".format(balance['last'])))
+    print("See you next time!")
 
 def welcome(balance) -> dict:
+    today = datetime.now()
     if len(yearList) == 0: # for new users
         print("New user.")
         while(True):
@@ -62,12 +42,11 @@ def welcome(balance) -> dict:
                 break
         balance['date'] = dateToFirst(balance['date'])
         balance['first'] = balance['last'] = float(input("Starting balance: "))
-        writeBalance(yearFilename(balance), balance)
+        createNewBalSheet(yearFilename(balance), balance)
+        print(balance)
+        writeBalance(0, balance)
     else: # for returning users
-        balance = readLine(yearList[-1])
-        #balance.date, balance.first, balance.last = readLine(path + yearList[-1])
-        print(balance['last'])
-        balance['date'] = balance['date']
+        balance = readLine(-1, needPrev = True)
     print("Current balance: $" + str("{:.02f}".format(balance['last'])))
     return balance
 
@@ -96,13 +75,12 @@ def newExpense(entry) -> dict:
     while(True):
         try:
             entry['date'] = dateTest(input("Entry date [MM/DD/YYYY]: "))
-            earliestBal = readLine(yearList[0], "earliest")
+            earliestBal = readLine(0, "earliest")
             if entry['date'] < earliestBal['date']:
                 print("Date precedes date of initial balance.")
                 raise
             break
-        except Exception as e:
-            print(e)
+        except:
             print("Please enter valid date.")
     entry['name'] = input("Entry name: ")
     print("""1. Food\n2. Shelter\n3. Fun\n4. Misc""")
@@ -114,7 +92,7 @@ def newIncome(entry) -> dict:
     while(True):
         try:
             entry['date'] = dateTest(input("Entry date [MM/DD/YYY]: "))
-            earliestBal = readLine(yearList[0], "earliest")
+            earliestBal = readLine(0, "earliest")
             if entry['date'] < earliestBal['date']:
                 print("Date precedes date of initial balance.")
                 raise
@@ -126,107 +104,93 @@ def newIncome(entry) -> dict:
     entry['amount'] = -float(input("Entry amount: "))
     return entry
 
-# the below code sets values for the newBalance entry/adjustment.
-def makeNewBalDict(entry, prevBalance) -> dict:
-    newBalance = prevBalance
-    newBalance['date'] = dateToFirst(entry['date'])
-    if newBalance['date'] == prevBalance['date']:
-        newBalance['first'] = prevBalance['first']
+# sets values for the newRow entry/adjustment.
+def updateRow(entry, oldRow) -> dict:
+    newRow = {}
+    newRow['date'] = dateToFirst(entry['date'])
+    if newRow['date'] == oldRow['date']:
+        newRow['first'] = oldRow['first']
     else:
-        newBalance['first'] = prevBalance['last']
-    newBalance['last'] = newBalance['last'] + entry['amount']
-    return newBalance
+        newRow['first'] = oldRow['last']
+    newRow['last'] = oldRow['last'] + entry['amount']
+    return newRow
+    
+# returns index of the newly created, empty file
+def createNewBalSheet(filename, initBal={}) -> int:
+    yearList.append(filename)
+    yearList.sort()
+    year = filename[slice(0,4)]
+    month = int(date[5:7]) # used for writing very first balance entry
+    with open(path + filename, 'a', newline='') as f:
+        fieldnames = ['date', 'first', 'last']
+        writer = csv.DictWriter(f, fieldnames = fieldnames, quoting=csv.QUOTE_NONNUMERIC)
+        writer.writeheader()
+        blankBal = {'date':'xxxx-01-xx', 'first':'X', 'last':'X'}
+        for i in range(month,13):
+            if initBal=={}:
+                blankBal['date'] = year + '-' + str("{:02d}".format(i)) + '-01'
+                writer.writerow(blankBal)
+            else:
+                writer.writerow(initBal)
+    return yearList.index(filename)
 
-    
-def getPrevBalSheet(filename):
-    if filename not in yearList:
-        print("getPrevBalSheet yearList: ")
-        print(yearList)
-        yearList.append(filename)
-        sorted(yearList)
-        return yearList[yearList.index(filename)-1]
-    else:
-        return filename
-    
-"""     for i in range(len(yearList)):
-        print(yearList[i])
-        if filename < yearList[i]:
-            print(yearList[i-1])
-            return yearList[i-1]
-        return yearList[i] """
-            
-# This is for finding an entry in the middle of a ####.csv file, inserting it,
-# and replacing all subsequent balance entries with entries mathed up to
-# reflect the changes.
-def writeBalance(filename, list, amount=0):
-    tempfile = NamedTemporaryFile('w+t', newline='', delete=False)
+def writeBalance(index, list, amount=0):
     fieldnames = ['date', 'first', 'last']
-    if filename not in yearList:
-        yearList.append(filename)
-        sorted(yearList)
-        with open(path + filename, 'a'): os.utime(path + filename, None)
-        print("this is where the file is created")
-        print("also here is the yearList: ")
-        print(yearList)
-    for i in range(len(yearList)):
-        if yearList[i] >= filename:
-            with open(path + yearList[i], 'r', newline='') as csvFile, tempfile:
-                reader = csv.DictReader(csvFile, fieldnames, quoting=csv.QUOTE_NONNUMERIC)
-                writer = csv.DictWriter(tempfile, fieldnames, quoting=csv.QUOTE_NONNUMERIC)
-                writer.writeheader()
-                next(reader, None)
-                for row in reader:
-                    if row['date'] > list['date']:
-                        writer.writerow(list)
-                        list['date'] = row['date']
-                        list['first'] = list['last']
-                        print(row['first'])
-                        list['last'] = row['first'] + amount
-                writer.writerow(list)
-            shutil.move(tempfile.name, path + filename)
-        else: print("if statement does not run")
+    for i in range(index, len(yearList)): # repeat for all years index and over
+        tempfile = NamedTemporaryFile('w+t', newline='', delete=False)
+        with open(path + yearList[i], 'r', newline='') as csvFile, tempfile:
+            reader = csv.DictReader(csvFile, fieldnames, quoting=csv.QUOTE_NONNUMERIC)
+            writer = csv.DictWriter(tempfile, fieldnames, quoting=csv.QUOTE_NONNUMERIC)
+            writer.writeheader()
+            next(reader, None)
+            for row in reader:
+                if row['date'] < list['date']: # copy existing row
+                    writer.writerow(row)
+                elif row['date'] == list['date']: # replace old row with new one
+                    writer.writerow(list)
+                elif row['date'] > list['date']: # update subsequent rows to reflect changes
+                    if isinstance(row['first'], float):
+                        row['first'] = row['first'] + amount
+                        row['last'] = row['last'] + amount
+                    writer.writerow(row)
+        shutil.move(tempfile.name, path + yearList[i])
 
 def writeEntry(filename, list) -> None:
     new = True if not os.path.exists(path + filename) else False
+    fieldnames = ['date', 'name', 'type', 'amount']
     with open(path + filename, 'a', newline='') as f:
-        fieldnames = ['date', 'name', 'type', 'amount']
         writer = csv.DictWriter(f, fieldnames = fieldnames, quoting=csv.QUOTE_NONNUMERIC)
         if new == True: writer.writeheader()
         writer.writerow(list)
 
-
-# If no date provided, reads last line of file
-def readLine(filename, date = '3000-01-01') -> dict:
-    with open(path + filename, 'r', newline='') as f:
+def readLine(index, date = "XXXX-XX-XX", needPrev = False) -> dict:
+    with open(path + yearList[index], 'r', newline='') as f:
         reader = csv.DictReader(f, quoting=csv.QUOTE_NONNUMERIC)
-        prevRow = {}
+        latestRow = {}
         for row in reader:
-            if date == "earliest":
+            if needPrev and row['date'] <= date: # previous edited row
+                if row['first'] != "X":
+                    latestRow = row
+            elif date == "earliest": # user can't input entries prior to earliest date
                 return row
-            elif row['date'] > date:
-                if prevRow == {}:
-                    print("PrevRow equaled nothing.")
-                    #return readLine(yearList[yearList.index(filename)-1], date)
-                return prevRow
-            prevRow = row
-        return prevRow
-            
-            
-# Checks that date is valid.
+            elif not needPrev and row['date'] == date: # return specific row
+                return row
+        if latestRow == {}:
+            latestRow = readLine(index - 1, date, needPrev)
+        return latestRow # for returning last used row of file
+                
 def dateTest(date = None) -> str:
     try:
         dtObject = datetime.strptime(date, "%m/%d/%Y")
-        print("Test dateTest function: " + str(dtObject.date()))
         return str(dtObject.date())
     except:
         print("Please enter valid date.")
-
+        
+# Entry dates need to be set to first of month to be compatible with balance sheet
 def dateToFirst(date) -> str:
     dateTemp = date
     date = dateTemp[:7] + "-01"
-    print("Adjusted date: " + date)
     return date
-    
     
 # File name format: YYYY-MM.csv
 def monthFilename(expenseDate) -> str:
@@ -235,7 +199,6 @@ def monthFilename(expenseDate) -> str:
     return filename
 
 # File name format: YYYY.csv
-# This file is for acct balance information
 def yearFilename(balanceDate) -> str:
     date = balanceDate['date']
     filename = date[:4] + ".csv"
