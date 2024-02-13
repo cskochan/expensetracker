104de83a4bb7cdec2df9a3dbcad594899d838ab6
Cameron <32208078+cskochan@users.noreply.github.com>
2024-02-12 16:42:37 -0800

    Delete expenses.py
    Deleted expenses.py. Decided to use dictionary instead of class

--- expenses.py
+++
@@ -1,6 +0,0 @@
-class Expenses:
-    def __init__(self, date = None, name = "unknown", type = "misc", amount = 0.0) -> None:
-        self.date = date
-        self.name = name
-        self.type = type
-        self.amount = amount
